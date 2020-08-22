2cae965b0f1e310294d3ff9ff333e25fff2ebee2
Zhang Yilin <e0377000@u.nus.edu>
2020-08-22 21:06:08 +0800

    Add a list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,46 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Duke {
+    static String [] thingsToDo=new String[100];
+    static int countThings=0;
+
+    public static void storeTextAndList(){
+        String line;
+        Scanner in=new Scanner(System.in);
+        line=in.nextLine();
+        while(!line.equals("bye")){
+            if(line.equals("list")){
+                printLine();
+                int count=0;
+                for(count=0;count<countThings;count++){
+                    System.out.print(String.format("%d. ",count+1));
+                    System.out.println(thingsToDo[count]);
+                }
+                printLine();
+            }
+            else{
+                printLine();
+                System.out.println("added: "+line);
+                printLine();
+                thingsToDo[countThings]=line;
+                countThings++;
+            }
+            line=in.nextLine();
+        }
+        bye();
+    }
+
+    public static void printLine(){
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void bye(){
+        printLine();
+        System.out.println("Bye. Hope to see you again soon");
+        printLine();
+    }
+
+
     public static void main(String[] args) {
         String initialGreet= "____________________________________________________________\n" +
                 " Hello! I'm Duke\n" +
@@ -7,18 +48,8 @@ public class Duke {
                 "____________________________________________________________\n";
 
         System.out.println(initialGreet);
-        echoCommand();
+        storeTextAndList();
     }
-    public static void echoCommand(){
-        String line;
-        Scanner in=new Scanner(System.in);
-        line=in.nextLine();
-        while(!line.equals("bye")){
-            System.out.print("____________________________________________________________\n" +line+
-                    "\n____________________________________________________________\n");
-            line=in.nextLine();
-        }
-        System.out.println("____________________________________________________________\n" +"Bye. Hope to see you again soon"+
-                "\n____________________________________________________________\n");
-    }
+
+
 }
