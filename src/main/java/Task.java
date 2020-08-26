977a373c77688a6088810a1cde0c2edf28fb66de
Zhang Yilin <e0377000@u.nus.edu>
2020-08-26 10:58:40 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public void taskDone(){
+        this.isDone=true;
+    }
+}
