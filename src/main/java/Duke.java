61a037a10f327de0d962a3b6ecc925635f689b5f
Zhang Yilin <e0377000@u.nus.edu>
2020-08-26 10:59:22 +0800

    Add mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,31 +3,41 @@ import java.util.Arrays;
 public class Duke {
     static String [] thingsToDo=new String[100];
     static int countThings=0;
+    static Task [] tasks=new Task[100];
 
     public static void storeTextAndList(){
         String line;
         Scanner in=new Scanner(System.in);
         line=in.nextLine();
         while(!line.equals("bye")){
+            printLine();
             if(line.equals("list")){
-                printLine();
                 int count=0;
                 for(count=0;count<countThings;count++){
                     System.out.print(String.format("%d. ",count+1));
-                    System.out.println(thingsToDo[count]);
+                    System.out.println(tasks[count].getStatusIcon()+thingsToDo[count]);
                 }
-                printLine();
+            }
+            else if(line.contains("done")){
+                int index=Integer.parseInt(line.substring(5));
+                doneTask(index);
             }
             else{
-                printLine();
                 System.out.println("added: "+line);
-                printLine();
                 thingsToDo[countThings]=line;
+                Task task= new Task(line);
+                tasks[countThings]=task;
                 countThings++;
             }
+            printLine();
             line=in.nextLine();
         }
         bye();
+    }
+
+    public static void doneTask(int index){
+        tasks[index].taskDone();
+        System.out.println("Nice! I've marked this task as done:\n" +tasks[index].getStatusIcon()+thingsToDo[index]);
     }
 
     public static void printLine(){
