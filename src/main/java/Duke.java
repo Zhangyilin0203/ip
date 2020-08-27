f414caaa604a7a6527848fe5fb4243f24e1f09e4
Zhang Yilin <e0377000@u.nus.edu>
2020-08-27 12:34:15 +0800

    fix a bug and adjusting the coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,43 +1,43 @@
 import java.util.Scanner;
 import java.util.Arrays;
 public class Duke {
-    static String [] thingsToDo=new String[100];
+    static String [] thingsToDo = new String[100];
     static int countThings=0;
-    static Task [] tasks=new Task[100];
+    static Task [] tasks = new Task[100];
 
     public static void storeTextAndList(){
         String line;
-        Scanner in=new Scanner(System.in);
-        line=in.nextLine();
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
         while(!line.equals("bye")){
             printLine();
             if(line.equals("list")){
-                int count=0;
+                int count = 0;
                 for(count=0;count<countThings;count++){
                     System.out.print(String.format("%d. ",count+1));
                     System.out.println(tasks[count].getStatusIcon()+thingsToDo[count]);
                 }
             }
             else if(line.contains("done")){
-                int index=Integer.parseInt(line.substring(5));
+                int index = Integer.parseInt(line.substring(5));
                 doneTask(index);
             }
             else{
                 System.out.println("added: "+line);
-                thingsToDo[countThings]=line;
-                Task task= new Task(line);
-                tasks[countThings]=task;
+                thingsToDo[countThings] = line;
+                Task task = new Task(line);
+                tasks[countThings] = task;
                 countThings++;
             }
             printLine();
-            line=in.nextLine();
+            line = in.nextLine();
         }
         bye();
     }
 
     public static void doneTask(int index){
-        tasks[index].taskDone();
-        System.out.println("Nice! I've marked this task as done:\n" +tasks[index].getStatusIcon()+thingsToDo[index]);
+        tasks[index-1].taskDone();
+        System.out.println("Nice! I've marked this task as done:\n" +tasks[index-1].getStatusIcon()+thingsToDo[index-1]);
     }
 
     public static void printLine(){
@@ -52,7 +52,7 @@ public class Duke {
 
 
     public static void main(String[] args) {
-        String initialGreet= "____________________________________________________________\n" +
+        String initialGreet = "____________________________________________________________\n" +
                 " Hello! I'm Duke\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n";
