1e57d2bdcfa61c24620242fc3858d1b41221bfef
Zhang Yilin <e0377000@u.nus.edu>
2020-08-29 18:56:21 +0800

    Add Todo Event and Deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,43 +1,83 @@
 import java.util.Scanner;
 import java.util.Arrays;
 public class Duke {
-    static String [] thingsToDo = new String[100];
     static int countThings=0;
     static Task [] tasks = new Task[100];
 
     public static void storeTextAndList(){
-        String line;
+        String inputLine;
         Scanner in = new Scanner(System.in);
-        line = in.nextLine();
-        while(!line.equals("bye")){
+        inputLine = in.nextLine();
+        while(!inputLine.equals("bye")){
             printLine();
-            if(line.equals("list")){
-                int count = 0;
+            if(inputLine.equals("list")){
+                int count;
                 for(count=0;count<countThings;count++){
-                    System.out.print(String.format("%d. ",count+1));
-                    System.out.println(tasks[count].getStatusIcon()+thingsToDo[count]);
+                    System.out.print(count+1 + ". ");
+                    System.out.println(tasks[count]);
+                }
+            } else if(inputLine.contains("done")){
+                int index = Integer.parseInt(inputLine.substring(5));
+                doneTask(index);
+            } else{
+                if(inputLine.contains("todo")){
+                    addTodoTask(inputLine);
+                }else if(inputLine.contains("deadline")){
+                    addDeadlineTask(inputLine);;
+                }else{
+                    addEventTask(inputLine);
                 }
             }
-            else if(line.contains("done")){
-                int index = Integer.parseInt(line.substring(5));
-                doneTask(index);
-            }
-            else{
-                System.out.println("added: "+line);
-                thingsToDo[countThings] = line;
-                Task task = new Task(line);
-                tasks[countThings] = task;
-                countThings++;
-            }
             printLine();
-            line = in.nextLine();
+            inputLine = in.nextLine();
         }
         bye();
     }
 
+    public static void addTodoTask(String inputLine){
+        String todoDescription;
+        todoDescription = inputLine.substring(5);
+        Task task = new Todo(todoDescription);
+        tasks[countThings] = task;
+        countThings++;
+        printTask(task);
+    }
+
+    public static void addDeadlineTask(String inputLine){
+        String deadlineDescription;
+        String deadlineByDate;
+        int getIndex;
+        getIndex = inputLine.indexOf("/");
+        deadlineDescription = inputLine.substring(9,getIndex-1);
+        deadlineByDate = inputLine.substring(getIndex+4);
+        Task task = new Deadline(deadlineDescription, deadlineByDate);
+        tasks[countThings] = task;
+        countThings++;
+        printTask(task);
+    }
+
+    public static void addEventTask(String inputLine){
+        String eventDescription;
+        String eventAtDate;
+        int getIndex;
+        getIndex = inputLine.indexOf("/");
+        eventDescription = inputLine.substring(6,getIndex-1);
+        eventAtDate = inputLine.substring(getIndex+4);
+        Task task = new Event(eventDescription, eventAtDate);
+        tasks[countThings] = task;
+        countThings++;
+        printTask(task);
+    }
+
+    public static void printTask(Task task){
+        System.out.println("Got it. I've added this task: ");
+        System.out.println(task);
+        System.out.println("Now you have " + countThings + " tasks in the list.");
+    }
+
     public static void doneTask(int index){
         tasks[index-1].taskDone();
-        System.out.println("Nice! I've marked this task as done:\n" +tasks[index-1].getStatusIcon()+thingsToDo[index-1]);
+        System.out.println("Nice! I've marked this task as done:\n" +tasks[index-1]);
     }
 
     public static void printLine(){
