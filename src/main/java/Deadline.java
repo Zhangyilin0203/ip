fc7e856d900ce899ab373c49c31ff7bab80520c6
Zhang Yilin <e0377000@u.nus.edu>
2020-08-29 18:53:38 +0800

    Add Deadline class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,17 @@
+public class Deadline extends Task {
+
+    protected String byDate;
+
+    public Deadline(String description, String byDate) {
+        super(description);
+        this.byDate=byDate;
+    }
+
+    public String getTypeIcon(){
+        return "[D]";
+    }
+
+    public String toString(){
+        return this.getTypeIcon() + this.getStatusIcon() + description + "(by: " + byDate + ")";
+    }
+}
