61a2ab4bf4639cb80e21b1f46e3b70f85ceac44a
Zhang Yilin <e0377000@u.nus.edu>
2020-08-29 18:54:03 +0800

    Add Event class

---
+++ src/main/java/Event.java
@@ -0,0 +1,16 @@
+public class Event extends Task {
+    protected String atDate;
+
+    public Event(String description, String atDate) {
+        super(description);
+        this.atDate=atDate;
+    }
+
+    public String getTypeIcon(){
+        return "[D]";
+    }
+
+    public String toString(){
+        return this.getTypeIcon() + this.getStatusIcon() + description + " (at: " + atDate + ")";
+    }
+}
